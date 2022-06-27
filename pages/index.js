f53f3d33d6f4ec0df3e6f3b0c27e5da9f3928f87
Chubop <samuelokirby@gmail.com>
2022-06-27 12:57:23 -0400

    added buttons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { Button, Grid } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import BottomNavBar from '../components/General/BottomNavBar'
 import NavBar from '../components/General/NavBar'
 
@@ -9,6 +11,14 @@ export default function Home() {
   return (
     <div>
       <NavBar/>
+      <Grid container>
+        <Link href="blakemasters">
+          <Button>Ask Blake Masters</Button>
+        </Link>
+        <Link href="respondent">
+          <Button>Answer Questions</Button>
+        </Link>
+      </Grid>
       <BottomNavBar/>
     </div>
   )
